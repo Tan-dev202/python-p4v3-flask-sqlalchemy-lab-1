2a4a50f205e16186efafb205a74c6dbc94ec8eec
Enock Tangus <tanguskipngeno@gmail.com>
2025-06-11 20:31:34 +0300

    flask sqlalchemy lab 1 complete

--- server/app.py
+++ server/app.py
@@ -23,5 +23,40 @@ def index():
 # Add views here
 
 
+@app.route('/earthquakes/<int:id>')
+def earhquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    if earthquake:
+        body = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        status = 200
+    else:
+        body = {"message": f'Earthquake {id} not found.'}
+        status = 404
+    return make_response(body, status)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquake_by_min_magnitude(magnitude):
+    earthquakes = []
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        earthquake_dict = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        earthquakes.append(earthquake_dict)
+    body = {'count': len(earthquakes),
+            'quakes': earthquakes
+            }
+
+    return make_response(body, 200)
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
